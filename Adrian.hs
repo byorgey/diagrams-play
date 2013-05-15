eff9d9844b2b3fe92ed9ff884124f39329859ffc
Brent Yorgey <byorgey@cis.upenn.edu>
2013-05-15 12:11:58 -0400

    improve example a bit

--- Adrian.hs
+++ Adrian.hs
@@ -7,6 +7,7 @@ import           Data.Maybe                     (fromMaybe)
 import           Diagrams.Backend.Cairo.CmdLine
 import           Diagrams.Prelude
 
+-- We can "mark" things just by giving them the name ()
 mark = named ()
 
 -- a bunch of stuff, each positioned according to its own sweet logic,
@@ -23,12 +24,20 @@ stuff = ( triangle 3 # mark
         |||
         ( square 2 # mark # translateY 2 )
 
+-- Draw horizontal lines extending from the right edges of any marked
+-- subdiagram to the given x-coordinate.  Extract all the marked
+-- subdiagrams using 'withNameAll ()', turn each into a function to
+-- draw the required line, and apply all of them.
 drawLinesTo x = withNameAll () $ \subs -> applyAll (map drawLineFrom subs)
   where
     drawLineFrom sub = atop (edgePt ~~ xPoint)
       where
+        -- Compute the point on the right edge of the subdiagram. This
+        -- is a little ugly at the moment; I hope to add combinators
+        -- to make this nicer.
         edgePt = fromMaybe origin (maxTraceP (location sub) unitX sub)
+        -- Compute the other endpoint of the segment.
         y      = snd (unp2 (location sub))
         xPoint = p2 (x,y)
 
-main = defaultMain (drawLinesTo 13 stuff # centerXY # pad 1.1)
+main = defaultMain (stuff # drawLinesTo 13 # centerXY # pad 1.1)
