8cffe2786699fe342cf24220c80c98fd7c8e7dd4
Brent Yorgey <byorgey@gmail.com>
2015-12-10 09:37:45 -0600

    add a bunch of stuff

--- Gabriel.hs
+++ Gabriel.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE PartialTypeSignatures     #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE TypeFamilies              #-}
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
 
-import Diagrams.Prelude
-import Diagrams.Backend.Cairo.CmdLine
-import System.Random
-import Control.Arrow ((>>>))
+import           Control.Arrow                       ((>>>))
+import           Diagrams.Backend.Rasterific.CmdLine
+import           Diagrams.Prelude
+import           System.Random
 
 -- convenient operator for use together with (>>>)
 infixr 0 >$>
@@ -11,13 +15,14 @@ infixr 0 >$>
 
 -- boxes takes as input the grout size, the total height, and a list
 -- specifying the number of squares in each column
+boxes :: _ -> _ -> [Int] -> Diagram B
 boxes grout ht = -- make a column from each #
-                 map (column grout ht)      >>>    
-                 
+                 map (column grout ht)      >>>
+
                  -- put the columns next to each other horizontally with
                  -- 'grout' amount of space between each
-                 hcat' with { sep = grout } >>>
-                 
+                 hsep grout >>>
+
                  -- center the whole thing
                  centerXY
 
@@ -25,18 +30,18 @@ boxes grout ht = -- make a column from each #
 -- of squares that should go in the column
 column grout ht n = -- make a list of squares
                     replicate n sq             >$>
-                    
+
                     -- lay them out vertically, with 'grout' space in between
-                    vcat' with { sep = grout } >>>
-                    
+                    vsep grout >>>
+
                     -- align at the top so the columns will line up properly
                     alignT
-                    
-             -- compute the proper square size       
+
+             -- compute the proper square size
   where sq = square ((ht - (n'-1)*grout) / n') # fc black
         n' = fromIntegral n
 
 main = do
   gen <- getStdGen
   let ns = take 70 $ randomRs (10,50) gen   -- generate some random numbers
-  defaultMain (pad 1.1 $ boxes 0.3 20 ns)   -- create the diagram
+  mainWith (pad 1.1 $ boxes 0.3 20 ns)   -- create the diagram
